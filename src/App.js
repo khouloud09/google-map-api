fa5d1a455be5a4962b6c6938b929eee5bb8afaa4
khouloud09 <khouloudlazrag90@gmail.com>
2022-10-20 23:04:32 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react"
+import MapContainer from "./components/mapContainer"
+
+function App() {
+  return (
+    <div className="grid-container">
+      
+           <MapContainer />
+       
+    </div>
+  );
+}
+
+export default App;
